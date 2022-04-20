bb292956bfcdc631ea2d17b4a37eecf6012bd9c6
Shaun Kirk Wong <skw@github.com>
2022-04-19 22:20:00 -0700

    Get project info and build dist

--- src/update-project-status.ts
+++ src/update-project-status.ts
@@ -1,3 +1,75 @@
-export async function updateProjectStatus(): Promise<void> {
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+
+// TODO: Ensure this (and the Octokit client) works for non-github.com URLs, as well.
+// https://github.com/orgs|users/<ownerName>/projects/<projectNumber>
+const urlParse =
+  /^(?:https:\/\/)?github\.com\/(?<ownerType>orgs|users)\/(?<ownerName>[^/]+)\/projects\/(?<projectNumber>\d+)/
+
+interface ProjectNodeIDResponse {
+  organization?: {
+    projectNext: {
+      id: string
+    }
+  }
+
+  user?: {
+    projectNext: {
+      id: string
+    }
+  }
 }
 
+export async function updateProjectStatus(): Promise<void> {
+  const projectUrl = core.getInput('project-url', {required: true})
+  const ghToken = core.getInput('github-token', {required: true})
+
+  const octokit = github.getOctokit(ghToken)
+  const urlMatch = projectUrl.match(urlParse)
+
+  core.debug(`Project URL: ${projectUrl}`)
+
+  if (!urlMatch) {
+    throw new Error(
+      `Invalid project URL: ${projectUrl}. Project URL should match the format https://github.com/<orgs-or-users>/<ownerName>/projects/<projectNumber>`
+    )
+  }
+
+  const ownerName = urlMatch.groups?.ownerName
+  const projectNumber = parseInt(urlMatch.groups?.projectNumber ?? '', 10)
+  const ownerType = urlMatch.groups?.ownerType
+  const ownerTypeQuery = mustGetOwnerTypeQuery(ownerType)
+
+  core.debug(`Org name: ${ownerName}`)
+  core.debug(`Project number: ${projectNumber}`)
+  core.debug(`Owner type: ${ownerType}`)
+
+  // First, use the GraphQL API to request the project's node ID.
+  const idResp = await octokit.graphql<ProjectNodeIDResponse>(
+    `query getProject($ownerName: String!, $projectNumber: Int!) { 
+      ${ownerTypeQuery}(login: $ownerName) {
+        projectNext(number: $projectNumber) {
+          id
+        }
+      }
+    }`,
+    {
+      ownerName,
+      projectNumber
+    }
+  )
+
+  const projectId = idResp[ownerTypeQuery]?.projectNext.id
+
+  core.debug(`Project node ID: ${projectId}`)
+}
+
+export function mustGetOwnerTypeQuery(ownerType?: string): 'organization' | 'user' {
+  const ownerTypeQuery = ownerType === 'orgs' ? 'organization' : ownerType === 'users' ? 'user' : null
+
+  if (!ownerTypeQuery) {
+    throw new Error(`Unsupported ownerType: ${ownerType}. Must be one of 'orgs' or 'users'`)
+  }
+
+  return ownerTypeQuery
+}
