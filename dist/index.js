78d5107e44679ab697865460c7ca071a144e7709
Shaun Kirk Wong <skw@github.com>
2022-04-22 16:54:30 -0700

    Fixes check and updates logs

--- dist/index.js
+++ dist/index.js
@@ -214,11 +214,10 @@ function projectItemsToUpdate({ projectItems, selectedStatusId, labeled }) {
     const formattedData = [];
     for (const projectItem of projectItems) {
         const statusFieldValue = projectItem.fieldValues.nodes.find(fieldValue => fieldValue.projectField.name === 'Status');
-        core.debug(`projectItem.content: ${JSON.stringify(projectItem.content)}`);
         const labels = ((_b = (_a = projectItem.content) === null || _a === void 0 ? void 0 : _a.labels) === null || _b === void 0 ? void 0 : _b.nodes.map(l => l.name)) || [];
-        core.debug(`labels: ${JSON.stringify(labels)}`);
-        const includesLabel = labeled.length > 0 && labels.length > 0 ? labels.some(l => labeled.includes(l)) : true;
+        const includesLabel = labeled.length > 0 ? labels.some(l => labeled.includes(l)) : true;
         if (includesLabel && statusFieldValue && (statusFieldValue === null || statusFieldValue === void 0 ? void 0 : statusFieldValue.value) !== selectedStatusId) {
+            core.debug(`adding item with labels: ${JSON.stringify(labels)}`);
             formattedData.push({
                 id: projectItem.id,
                 statusValue: statusFieldValue === null || statusFieldValue === void 0 ? void 0 : statusFieldValue.value
